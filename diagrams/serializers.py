280705bf699a409212dcd2cbfe208dcd06d2c145
tanya <trunova812@gmail.com>
2025-10-14 11:46:57 +0300

    project serializer

--- diagrams/serializers.py
+++ diagrams/serializers.py
@@ -1,1 +1,10 @@
-from rest_framework import
+from rest_framework import serializers
+from models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="user.username", read_only=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
