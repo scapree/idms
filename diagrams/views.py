79b9e8c01e460af00eef0cf2dd207f6806d1369c
tanya <trunova812@gmail.com>
2025-11-08 16:49:24 +0300

    diagram view

--- diagrams/views.py
+++ diagrams/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
 
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import ProjectSerializer, UserRegistrationSerializer
-from .models import Project
+from .serializers import ProjectSerializer, UserRegistrationSerializer, DiagramSerializer
+from .models import Project, Diagram
 
 
 
@@ -49,7 +50,6 @@ def login_user(request):
 
 class ProjectApiView(generics.ListCreateAPIView):
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
 
     def get_queryset(self):
         return Project.objects.filter(user = self.request.user)
@@ -58,12 +58,48 @@ class ProjectApiView(generics.ListCreateAPIView):
         serializer.save(user = self.request.user)
 
 
-class ProjectDelailApiView(generics.RetrieveAPIView):
-    queryset = Project.objects.all()
+class ProjectDetailApiView(generics.RetrieveAPIView):
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
     
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    def get(self, request, project_id):
+        project = Project.objects.get(id=project_id)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class DiagramApiView(generics.ListCreateAPIView):
+    serializer_class = DiagramSerializer
+
+    def get_queryset(self):
+        project_id = self.kwargs["project_id"]
+        return Diagram.objects.filter(project = project_id)
+    
+    def perform_create(self, serializer):
+        project = Project.objects.get(id=self.kwargs["project_id"])
+        serializer.save(project=project, data={}, locked_by=None, is_locked=False)
+
+
+class DiagramDetailApiView(APIView):
+    serializer_class = DiagramSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, diagram_id):
+        diagram = Diagram.objects.get(id=diagram_id)
+        serializer = DiagramSerializer(diagram)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, diagram_id):
+        diagram = Diagram.objects.get(id=diagram_id)
+        serializer = DiagramSerializer(diagram, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, diagram_id):
+        diagram = Diagram.objects.get(id=diagram_id)
+        diagram.delete()
+        return Response(status=204)
     
