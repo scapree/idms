5b4a4607018b2a09a51052a5fe640abccba57b0b
Valter <valter@7098487-tb401050.twc1.net>
2026-03-24 06:40:46 +0000

    host on server

--- diagram_system/settings.py
+++ diagram_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mf@sm#q*9jjpy@egf_9lx9&tw85j-uc#&5!g4&e=#e*biet-7q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['idms.space', 'www.idms.space', 'localhost', '127.0.0.1', 'testserver']
 
 
 # Application definition
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['idms.space', 'www.idms.space'],
     port: 5173,
     proxy: {
       '/api': {
